c655006d2e2c5b10375d454a260f3debc9c1ae21
beto1821 <beto1821@uol.com.br>
2022-08-16 23:28:56 -0300

    desafio3 rev01

--- desafio3.sql
+++ desafio3.sql
@@ -1,13 +1,16 @@
 -- Active: 1660079668896@@127.0.0.1@3306@SpotifyClone
-SELECT 
+
+SELECT
     p.person_name AS usuario,
-    h.history_music_id  AS qt_de_musicas_ouvidas,
-    SUM(m.music_duration /60) AS total_minutos
-FROM 
-    person AS p,
-    history AS h,
-    music AS m
-ORDER BY p.person;
+    h.history_music_id AS qt_de_musicas_ouvidas,
+    ROUND(SUM(m.music_duration) / 60, 2) AS total_minutos
+FROM SpotifyClone.person AS 
+    INNER JOIN SpotifyClone.history AS h ON p.person_id = h.history_person_id
+    INNER JOIN SpotifyClone.music AS m ON m.musci_id = h.history_musci_id
+ORDER BY p.person_name
+ORDER BY p.person_name ASC;
 
 
-
+-- SELECT
+--     ROUND(SUM(music_duration) / 60, 2) AS total_minutos
+-- FROM SpotifyClone.music;
