75f3397a66ca91d13c27fdc7243e8ed00c924688
beto1821 <beto1821@uol.com.br>
2022-08-16 22:03:20 -0300

    desafio2 rev2

--- desafio02.sql
+++ desafio02.sql
@@ -1,2 +1,8 @@
--- Active: 1660079668896@@127.0.0.1@3306@SpotifyClone
-SELECT 
+SELECT
+    COUNT(DISTINCT m.music_id) cancoes,
+    COUNT(DISTINCT ar.artist_id) artistas,
+    COUNT(DISTINCT al.album_id) albuns
+FROM
+    SpotifyClone.music AS m,
+    SpotifyClone.artist AS ar,
+    SpotifyClone.album AS al;
