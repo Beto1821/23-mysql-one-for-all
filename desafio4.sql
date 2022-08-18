54824cc11d3ad34a9cb568b8c9356c779005de39
beto1821 <beto1821@uol.com.br>
2022-08-18 09:37:04 -0300

    desafio4 rev1

--- desafio4.sql
+++ desafio4.sql
@@ -1,6 +1,13 @@
 -- Active: 1660079668896@@127.0.0.1@3306@SpotifyClone
 
-SELECT 
-    person_name AS usuario, 
-    IF( YEAR(person_signature_date) >= 2021, 'Usuário ativo', 'Usuário inativo') AS status_usuario 
-FROM SpotifyClone.person;
+SELECT
+    p.person_name AS usuario,
+    IF(
+        YEAR(MAX(h.history_reproduction_date)) >= 2021,
+        'Usuário ativo',
+        'Usuário inativo'
+    ) AS status_usuario
+FROM SpotifyClone.person AS p
+    INNER JOIN SpotifyClone.history AS h ON p.person_id = h.history_person_id
+GROUP BY person_name    
+ORDER BY person_name ASC;
